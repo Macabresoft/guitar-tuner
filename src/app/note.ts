100d960f5300cd0d455e32aeb9507181d98502cf
Brett Story <brettmstory@gmail.com>
2023-07-14 16:55:55 -0500

    Basic note detection

---
+++ src/app/note.ts
@@ -0,0 +1,7 @@
+export interface Note {
+  frequency: number;
+  halfStepDown: number;
+  halfStepUp: number;
+  name: string;
+  octave: number;
+}
--- src/app/tuner/tuner.component.ts
+++ src/app/tuner/tuner.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FrequencyService } from '../frequency.service';
+import { Note } from '../note';
+import { BufferInformation } from '../buffer-information';
 
 @Component({
   selector: 'app-tuner',
@@ -8,10 +10,20 @@ import { FrequencyService } from '../frequency.service';
   providers: [FrequencyService]
 })
 export class TunerComponent {
-  minimumFrequency = 66.0;
-  maximumFrequency = 392.0;
+  private readonly minimumFrequency = 60.0;
+  private readonly maximumFrequency = 392.0;
+  private readonly notes: Note[] = [
+    { frequency: 73.42, halfStepDown: 69.30, halfStepUp: 77.78, name: 'D', octave: 2 },
+    { frequency: 82.41, halfStepDown: 77.78, halfStepUp: 87.31, name: 'E', octave: 2},
+    { frequency: 110.00, halfStepDown: 103.83, halfStepUp: 116.54, name: 'A', octave: 2 },
+    { frequency: 146.83, halfStepDown: 138.59, halfStepUp: 155.56, name: 'D', octave: 3 },
+    { frequency: 196.00, halfStepDown: 185.00, halfStepUp: 207.65, name: 'G', octave: 3 },
+    { frequency: 246.94, halfStepDown: 233.08, halfStepUp: 261.63, name: 'B', octave: 3 },
+    { frequency: 329.63, halfStepDown:  311.13, halfStepUp: 349.23, name: 'E', octave: 4 }
+  ];  
+
   currentFrequency = '';
-  currentVolume = '';
+  currentNote?: Note;
 
   constructor(private frequencyService: FrequencyService) {  
   }
@@ -25,13 +37,31 @@ export class TunerComponent {
     }
   }
 
-  update() : void {
-    let bufferInformation = this.frequencyService.GetBufferInformation();
-    this.currentFrequency = bufferInformation.frequency.toFixed(2);
-    this.currentVolume = bufferInformation.volume.toFixed(10);
+  private async getMediaStream(): Promise<MediaStream> {
+    return await navigator.mediaDevices.getUserMedia({ audio: true });
   }
 
-  async getMediaStream(): Promise<MediaStream> {
-    return await navigator.mediaDevices.getUserMedia({ audio: true });
+  private getNearestNote(bufferInformation: BufferInformation) : Note {
+    let result = { frequency: 0, halfStepDown: -Infinity, halfStepUp: -Infinity, name: '', octave: -Infinity};
+    
+    for (let i = 0; i < this.notes.length; i++) {
+      let note = this.notes[i];
+
+      if (bufferInformation.frequency > note.halfStepDown && bufferInformation.frequency < note.halfStepUp) {
+        result = note;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  private update() : void {
+    let bufferInformation = this.frequencyService.GetBufferInformation();
+    this.currentFrequency = bufferInformation.frequency.toFixed(2);
+    let currentNote = this.getNearestNote(bufferInformation);
+    if (currentNote.frequency > 0) {
+      this.currentNote = currentNote;
+    }
   }
 }
